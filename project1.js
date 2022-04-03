28c14005409b30bfa6c3f97dbb26a75f1d19a353
aayush <aayush.20bcrn010@jecrcu.edu.in>
2022-04-04 01:40:01 +0530

    time changes

--- project1.js
+++ project1.js
@@ -1,4 +1,9 @@
-a = new Date();
-let date = a.toLocaleDateString();
-let time = a.getHours() + ':' + a.getMinutes() + ':' + a.getSeconds();
-document.getElementById('time').innerHTML = time + ' on ' + date;
+// setInterval(() => {
+taketime = () => {
+        a = new Date();
+        let date = a.toLocaleDateString();
+        let time = a.getHours() + ':' + a.getMinutes() + ':' + a.getSeconds();
+        document.getElementById('time').innerHTML = time + ' on ' + date;
+    }
+    // }, 1000);
+setInterval(taketime, 1000);
