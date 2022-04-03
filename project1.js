e238c26a54e7ae81b4bbee4815166d40de27afe0
aayush <aayush.20bcrn010@jecrcu.edu.in>
2022-04-03 23:45:48 +0530

    Created clock using JS

---
+++ project1.js
@@ -0,0 +1,4 @@
+a = new Date();
+let date = a.toLocaleDateString();
+let time = a.getHours() + ':' + a.getMinutes() + ':' + a.getSeconds();
+document.getElementById('time').innerHTML = time + ' on ' + date;
